8e239f1ab7a2a3b7c8106a0cca83e40e3e39c576
Badal Soni <111721917+BadalSoni03@users.noreply.github.com>
2023-10-24 23:19:54 +0530

    Add files via upload

---
+++ Controllers/authController.js
@@ -0,0 +1,115 @@
+const JWT = require("jsonwebtoken");
+const User = require("../Models/UserModel");
+
+const registerController = async function (req , res) {
+	const {username , email , password} = req.body;
+	const isNewUser = await User.newUserOrNot(email);
+	if (!isNewUser) {
+		return res.status(401).send({
+			success : false,
+			message : 'User already registered'
+		});
+	}
+
+	const newUser = await User.create({
+		username,
+		email,
+		password
+	});
+	if (newUser) {
+		return res.status(201).send({
+			success : true,
+			message : 'User registered successfully',
+			newUser
+		});
+	} else {
+		return res.status(501).send({
+			success : false,
+			message : 'Error in register API'
+		});
+	}
+};
+
+const loginController = async function (req , res) {
+	const {email , password} = req.body;
+	const user = await User.findOne({email});
+	if (!user) {
+		return res.status(401).send({
+			success : false,
+			message : 'User not registered'
+		});
+	}
+	const isMatch = user.comparePassword(password);
+	if (!isMatch) {
+		return res.status(401).send({
+			success : false,
+			message : 'email / password not matching',
+		});
+	}
+
+	const token = JWT.sign({userId : user._id} , process.env.JWT_SECRET_KEY , {expiresIn : '1d'});
+	let oldTokens = user.tokens || [];
+	
+	if (oldTokens.length) {
+		oldTokens = oldTokens.filter(tkn => {
+			const timeDiff = (Date.now() - parseInt(tkn.signedAt)) / 1000;
+			if (timeDiff < 86400) {
+				return tkn;
+			}
+		});
+	}
+
+	await User.findByIdAndUpdate(user._id , {
+		tokens : [...oldTokens , {
+			token,
+			signedAt : Date.now().toString()
+		}]
+	});
+	const info = {
+		Username : user.username,
+		email : user.email,
+		token
+	};
+	return res.status(201).send({
+		success : true,
+		message : 'User logged in successfully',
+		info
+	});
+};
+
+const logoutController = async function (req , res) {
+	try {
+		if (req.headers && req.headers.authorization) {
+			const token = req.headers.authorization.split(' ')[1];
+			if (!token) {
+				return res.status(501).send({
+					success : false,
+					message : 'Authorization failed'
+				});
+			}
+			const tokens = req.user.tokens;
+			const notExpiredTokens = tokens.filter(t => t.token !== token);
+			await User.findByIdAndUpdate(req.user._id , {tokens : notExpiredTokens});
+			return res.status(201).send({
+				success : true,
+				message : 'logged out successfully'
+			});
+		} else {
+			return res.status(501).send({
+				success : false,
+				message : 'Cannot logout'
+			});
+		}
+	} catch (error) {
+		return res.status(501).send({
+			success : false,
+			message : 'Error in logout API'
+		});
+	}
+};
+
+module.exports = {
+	registerController,
+	loginController,
+	logoutController
+};
---
+++ Controllers/blogController.js
@@ -0,0 +1,95 @@
+const Blog = require('../Models/BlogModel');
+const User = require('../Models/UserModel');
+const mongoose = require('mongoose');
+
+// POST Controllers
+
+const createBlogController = async function (req , res) { 
+	try {
+		const {title , body , author} = req.body;
+		const currentUser = await User.findOne({username : author});
+		if (!currentUser) { 
+			console.log(author);
+			return res.status(401).send({
+				success : false,
+				message : 'Not Allowed'
+			});
+		}
+		const timeOfCreation = Date.now();
+		const like = 0;
+		const blog = new Blog({
+			title,
+			body,
+			author,
+			like,
+			timeOfCreation
+		});
+
+		currentUser.blogs.push(blog);
+		return res.status(201).send({
+			success : true,
+			message : 'Blog created successfully',
+			blog
+		});
+	} catch (error) {
+		return res.status(501).send({
+			success : false,
+			message : 'Error in create API',
+			error : error.message
+		});
+	}
+};
+
+const likeBlogController = async function (req , res) {
+	try {
+		const uniqueID = req.params.id; 
+		const blog = await Blog.findById(uniqueID);
+		blog.likes++;
+		await blog.save();
+		return res.status(201).send({
+			success : true,
+			message : 'successfully liked the blog'
+		});
+	} catch (error) {
+		return res.status(501).send({
+			success : false,
+			message : 'Error in likeBlogController API',
+			error : error.message
+		});
+	}
+}
+
+
+// GET controllers
+const getAllBlogsOfAUserController = async function (req , res) {
+	try {
+		const {author} = req.body;
+		const userFound = await User.findOne({username : author});
+		if (!userFound) {
+			return res.status(401).send({
+				success : false,
+				message : 'User of such username does not exist'
+			});
+		}
+
+		const allBlogs = await Blog.find({author}); 
+		return res.status(201).send({
+			success : true,
+			message : 'Blogs fetched successfully',
+			allBlogs
+		});
+	} catch (error) {
+		return res.status(501).send({
+			success : false,
+			message : 'Error in getAllBlogsOfAUser API',
+			error : error.message
+		});
+	}
+};
+
+
+module.exports = {
+	createBlogController,
+	getAllBlogsOfAUserController,
+	likeBlogController
+};
