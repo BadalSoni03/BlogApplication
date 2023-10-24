f12bf2b91baeb15d1a463bb89332ef3f73f3e8ed
Badal Soni <111721917+BadalSoni03@users.noreply.github.com>
2023-10-24 23:22:08 +0530

    server setup

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const dotenv = require("dotenv"); 
+const cors = require("cors");
+const connectToDB = require("./Config/db");
+const authRouter = require('./Routes/authRouter');
+const blogRouter = require('./Routes/blogRouter');
+dotenv.config();
+connectToDB()
+
+const port = process.env.port || 8080;
+const app = express(); 
+
+app.use(express.json());
+app.use(cors());
+app.listen(port , () => {
+	console.log("Server is running on http://localhost:" + port);
+});
+
+
+app.use('/auth' , authRouter);
+app.use('/blog' , blogRouter);
