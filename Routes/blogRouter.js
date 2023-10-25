3877fdaefdf594066381d1b0e37f4f129d88b3b9
Badal Soni <111721917+BadalSoni03@users.noreply.github.com>
2023-10-25 22:48:29 +0530

    route for fetching all users in the DB created successfully

--- Routes/blogRouter.js
+++ Routes/blogRouter.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const isAuth = require('../Middlewares/isAuth');
+const allowedToAdmin = require('../Middlewares/allowedToAdmin');
 const {
 	createBlogController,
 	getAllBlogsOfAUserController,
-	likeBlogController
+	likeBlogController,
+	getAllUsersController
 } = require('../Controllers/blogController');
 
 //-------------------------POST APIs-------------------------//
+
 router.post('/create' , isAuth , createBlogController);
 router.post('/like/:username/:id' , isAuth , likeBlogController);
 
-//-------------------------GET APIs--------------------------//
-router.get('/fetch/:username', getAllBlogsOfAUserController);
 
+//-------------------------GET APIs--------------------------//
+
+router.get('/fetch/:username', getAllBlogsOfAUserController);
+router.get('/all-users', allowedToAdmin , getAllUsersController);
 
 module.exports = router;
